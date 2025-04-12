a83212ad7e3c81f0a8467bcf6bcda0c3dfd7276b
Nicolás Palacios <niconebu12@gmail.com>
2025-04-12 18:47:46 -0300

    Ejercicio 3 resuelto por Nicolas Palacios

---
+++ src/ejercicio3.js
@@ -0,0 +1,8 @@
+//3. Calcular y mostrar por consola el área de un círculo cuyo radio = 7,5
+
+let radioCirculo = 7.5;
+area = Math.PI * (radioCirculo ** 2)
+
+console.log(`El área del circulo es: ${area}`);
+
+

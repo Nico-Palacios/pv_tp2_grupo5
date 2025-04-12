5a6b13bf2d60b15c7a92ea100a903339e660dd74
prieto555 <prietogabriel555@gmail.com>
2025-04-12 19:05:21 -0300

    Ejercicio4 resuelto por GabrielPrieto

---
+++ src/ejercicio2.js
@@ -0,0 +1,6 @@
+let Lcuadrado = 10;
+let Acuadrado = Lcuadrado * Lcuadrado;
+let Pcuadrado = Lcuadrado * 4;
+console.log('Lado del cuadrado: ', Lcuadrado);
+console.log('Area del cuadrado:', Acuadrado);
+console.log('Perimetro del cuadrado',Pcuadrado);

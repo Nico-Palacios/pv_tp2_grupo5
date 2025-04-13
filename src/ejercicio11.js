859dd1199e170d74df00569dd95c901f3b245cc3
VenancioCopa <venanciocopa@gmail.com>
2025-04-13 14:11:57 -0300

    Ejercicio 11 resuelto por Venancio Copa

---
+++ src/ejercicio11.js
@@ -0,0 +1,6 @@
+let kilometro = prompt ("ingrese los kilometros: ");
+let combustible = prompt ("ingrese los litros de combustibles que uso: " );
+let consumototal = combustible / kilometro ;
+alert("Su consumo de combustible por kilómetro es "+consumototal);
+console.log("Su consumo de combustible por kilómetro es ",consumototal);
+
